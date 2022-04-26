dac9e91797c580c5e5a7d9641642d0ff51529859
MarcoPieretti <marcopieretti50@gmail.com>
2022-04-26 16:42:38 +0200

    Esercizio completato

---
+++ ListaIndirizzi/Address.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Address
+    {
+        private string nome;
+        private string cognome;
+        private string indirizzo;
+        private string citta;
+        private string provincia;
+        private string cap;
+
+        public Address(string nome, string cognome, string indirizzo, string citta, string provincia, string cap)
+        {
+            ControlloElementi(nome);
+            this.nome = nome;
+            ControlloElementi(cognome);
+            this.cognome = cognome;
+            ControlloElementi(indirizzo);
+            this.indirizzo = indirizzo;
+            ControlloElementi(citta);
+            this.citta = citta;
+            ControlloElementi(provincia);
+            this.provincia = provincia;
+            ControlloElementi(cap);
+            this.cap = cap;
+        }
+
+        //--------------GETTERS--------
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public string GetCognome()
+        {
+            return cognome;
+        }
+
+        public string GetIndirizzo()
+        {
+            return indirizzo;
+        }
+
+        public string GetCitta()
+        {
+            return citta;
+        }
+
+        public string GetProvincia()
+        {
+            return provincia;
+        }
+
+        public string GetCap()
+        {
+            return cap;
+        }
+        //----------------------------------
+
+
+        //----------SETTER--------------
+        public void SetCap(string cap)
+        {
+            bool canConvert = int.TryParse(cap, out int val);
+            if(canConvert == true && cap.Length == 5)
+            {
+                this.cap = cap;
+            } 
+            else
+            {
+                throw new Exception("Non hai inserito numeri");
+            }
+        }
+
+        //Controllo se sono stringhe null
+        private static void ControlloElementi(string controllo)
+        {
+            if (controllo == "")
+            {
+                throw new Exception("Elemento non inserito");
+            } 
+        }
+
+        //Override metodo toString per stampare gli elementi del file
+        public override string ToString()
+        {
+            string rapresentazioneInStringa = "------- Informazioni Personali -------\n";
+            rapresentazioneInStringa += "Nome:\t" + this.nome + "\n";
+            rapresentazioneInStringa += "Cognome:\t" + this.cognome + "\n";
+            rapresentazioneInStringa += "Indirizzo:\t" + this.indirizzo + "\n";
+            rapresentazioneInStringa += "Citta:\t" + this.citta + "\n";
+            rapresentazioneInStringa += "Provincia:\t" + this.provincia + "\n";
+            rapresentazioneInStringa += "CAP:\t" + this.cap + "\n";
+            rapresentazioneInStringa += "---------------------";
+            return rapresentazioneInStringa;
+        }
+
+    }
+}
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using ListaIndirizzi;
+
+string path = "C:\\Users\\marco\\source\\repos\\ListaIndirizzi\\ListaIndirizzi\\addresses.csv";
+
+List<Address> indirizzi = new List<Address>();
+
+StreamReader file = new StreamReader(path);
+
+file.ReadLine();
+while (!file.EndOfStream)
+{
+    string riga = file.ReadLine();
+    string[] informazionePerDividere = riga.Split(',');
+      
+    if (informazionePerDividere.Length == 6)
+    {
+        try
+        {  
+            string nome = informazionePerDividere[0];
+            string cognome = informazionePerDividere[1];
+            string indirizzo = informazionePerDividere[2];
+            string citta = informazionePerDividere[3];
+            string provincia = informazionePerDividere[4];
+            string cap = informazionePerDividere[5];
+
+            indirizzi.Add(new Address(nome, cognome, indirizzo, citta, provincia, cap));
+
+            Console.WriteLine();
+        } 
+        catch(Exception ex) 
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Elementi inseriti non correttamente!");
+    }
+}
+
+file.Close();
+
+foreach(Address indirizzo in indirizzi)
+{
+    Console.WriteLine(indirizzo.ToString());
+    Console.WriteLine();    
+}
